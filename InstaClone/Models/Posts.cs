bc3250f81ea12dcfee5f8eed15db464b84d52a62
Hesam Fatehi <hesamfatehi8@gmail.com>
2021-08-08 01:19:09 +0430

    Added some basic user models

---
+++ InstaClone/Models/Posts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InstaClone.Models
+{
+    public class Posts
+    {
+        public int PostId { get; set; }
+
+        public int UserId { get; set; }
+
+
+        public Users User { get; set; }
+    }
+}
---
+++ InstaClone/Models/Users.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InstaClone.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public DateTime RegisterDate { get; set; }
+
+
+
+        public IEnumerable<Posts> Posts { get; set; }
+    }
+}
